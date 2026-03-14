22fc95be50c03ed2d365eed9aaeaec1f2498d86c
medhanshmallik1-sys <medhanshmallik1@gmail.com>
2026-03-14 09:15:27 +0530

    homework

---
+++ homework.py
@@ -0,0 +1,17 @@
+dis1 = int(input("Enter a ride 1 for bike and 2 for car :"))
+choise1= dis1
+if choise1 == 1:
+    s = int(input("Enter bike you want : 1 for pulsar and 2 for apache :"))
+    if s == 2:
+        print("You have selected apache")
+    elif s == 1:
+        print("You have selected pulsar")
+if choise1 == 2:
+    s = int(input("Enter car you want : 1 for sedan and 2 for suv :"))
+    if s == 2:
+        print("You have selected suv")
+    elif s == 1:
+        print("You have selected sedan")
+print("THANK YOU FOR CHOOSING US")
+print("your ride is on the way")
+    
